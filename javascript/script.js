e41075034b54f2115192fd00382ab2c59406ce10
Ahmed Elmakrini <almakrini2003@gmail.com>
2026-06-19 19:13:05 +0100

    fixing the email input validation

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-let users = []
+let users = JSON.parse(localStorage.getItem('users')) || []
 let action = prompt('Welcome! Choose an option:\n1. Sign Up\n2. Exit')
 switch (action) {
   case '1':
@@ -14,45 +14,47 @@ switch (action) {
       if (/[^a-zA-Z ]/.test(name))
         return 'Name can only contain letters and spaces.'
       let correctedName = name
-        .split(' ')
-        .map(word => word[0].toUpperCase() + word.slice(1).toLowerCase())
-        .join(' ')
+      .split(' ')
+      .map(word => word[0].toUpperCase() + word.slice(1).toLowerCase())
+      .join(' ')
       return correctedName
     }
     // ! this function for valid email verification
+    function emailExists (email) {
+      return users.some(user => user.email === email)
+    }
     function validEmail (email) {
       if (!email) return 'Email cannot be empty.'
       email = email.trim().toLowerCase()
-      if (email.length < 10) return 'Email must contain at least 10 characters.'
-      if (!email.includes('@')) return 'Email must contain @'
+      if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+        return 'Enter a valid email example \n like: exemple@gmail.com'
+      }
+      if (emailExists(email)) return 'Email already used.'
       return email
     }
     let fullName
     let email
     let step = 1
-
     while (true) {
-      // ?this step 1 for name 
+      // ? this step 1 for loop fullname validation 
       if (step === 1) {
         fullName = prompt('Please enter your full name:')
         let result = validName(fullName)
-
         if (!result.includes('Name')) {
           fullName = result
           step = 2
         } else {
           alert(result)
         }
-      }
-      // ?this step for the email 
-      else if (step === 2) {
+        // ? this step 2 for loop email validation 
+      } else if (step === 2) {
         email = prompt('Please enter your email:')
         let result = validEmail(email)
-        if (!result.includes('Email')) {
-          email = result
-          break // stop loop here
-        } else {
+        if (result.includes('Email') || result.includes('valid email')) {
           alert(result)
+        } else {
+          email = result
+          break
         }
       }
     }
@@ -61,6 +63,7 @@ switch (action) {
       fullName: fullName,
       email: email
     })
+    localStorage.setItem('users', JSON.stringify(users))
     console.log('User saved:', users)
     break
   case '2':
